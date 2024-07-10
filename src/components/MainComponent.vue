83a685613e272f943537772cdda3ea0420bd4816
vohidimustafo <vohidiprofticket@gmail.com>
2024-07-10 17:33:31 +0300

    установил таймер

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -5,16 +5,45 @@ const global=useGlobalSettings()
 const startedindex = ref(0)
 const timerGrouppedByDate=reactive([])
 const timerListing=reactive([])
+const thissetin=ref(null)
 function startTimer(timer){
   startedindex.value=timer.timerid
-
+  let duration = timer.duration.split(":")
+  let hour=Number(duration[0])
+  let minutes=Number(duration[1])
+  let seconds=Number(duration[2])
+  thissetin.value = setInterval(()=>{
+    const currenttimeout = hour*60*60 + minutes*60 + seconds
+    console.log(currenttimeout, Number(global.timeout)*60)
+    if(currenttimeout === global.timeout*60){
+      stopTimer(timer)
+    }else{
+      if(seconds===0){
+        if(minutes===0){
+          if(hour===0){
+            stopTimer(timer)
+          }else {
+            hour--
+            minutes=59
+          }
+        }else{
+          minutes--
+          seconds=59
+        }
+      }else {
+        seconds --
+      }
+    }
+    timer.duration = `${hour}:${minutes.toString().length===1? '0'+minutes : minutes}:${seconds.toString().length===1? '0'+seconds : seconds}`
+  }, 1000)
 }
 function stopTimer(timer){
+  console.log('set')
   const index=timerGrouppedByDate.indexOf(timer)
-  console.log(timerGrouppedByDate.indexOf(timer), timer)
   timerGrouppedByDate.splice(index, 1)
   timerListing.push(timer)
   startedindex.value=0
+  clearInterval(thissetin.value)
 }
 function pauseTimer(timer){
   console.log(timer, this)
@@ -39,85 +68,150 @@ onMounted(async ()=>{
 </script>
 <template>
   <div class="timer-wrapper">
-    <div class="timer-listing" v-if="!!timerListing.length">
-      <div class="title">
-        Завершенные таймеры
-      </div>
+    <div class="title-timeout">
+      Установленный таймаут {{global.timeout}} мин.
     </div>
-    <div class="timer-list">
-      <div class="timer-head">
-        <div class="id-timer">
-          №
+    <div class="timer-closed">
+      <div class="timer-list" v-if="!!timerListing.length">
+        <div class="title">
+          Завершенные таймеры
         </div>
-        <div class="timer-date-start">
-          Дата начало
-        </div>
-        <div class="timer-date-end">
-          Дата окончания
-        </div>
-        <div class="timer-duration">
-          Длительность
-        </div>
-        <div class="custom-buttons">
-
-        </div>
-      </div>
-      <div class="timer-body">
-        <div class="timer-item" v-for="timer in timerGrouppedByDate" :key="timer.id">
+        <div class="timer-head">
           <div class="id-timer">
-            {{timer.timerid}}
+            №
           </div>
-          <div class="timer-date-start">
-            {{timer.datestart}}
-          </div>
-          <div class="timer-date-end">
-            {{timer.dateend}}
+          <div class="id-timer">
+            ID
           </div>
           <div class="timer-duration">
-            {{timer.duration}}
+            Время заверщения
           </div>
-          <div class="custom-buttons">
-            <el-button
-                @click="startTimer(timer)"
-                class="start"
-                type="primary"
-                plain
-                :disabled="startedindex && timer.timerid!==startedindex"
-            >
-              Старт
-            </el-button>
-            <el-button
-                @click="stopTimer(timer)"
-                class="start"
-                type="danger"
-                plain
-                :disabled="startedindex && timer.timerid!==startedindex"
-            >
-              Стоп
-            </el-button>
-            <el-button
-                @click="pauseTimer(timer)"
-                class="start"
-                type="warning"
-                plain
-                :disabled="startedindex && timer.timerid!==startedindex"
-            >
-              Пауза
-            </el-button>
+        </div>
+        <div class="timer-body">
+          <div class="timer-item" v-for="(timer, index) in timerListing" :key="timer.id">
+            <div class="id-timer">
+              {{ index }}
+            </div>
+            <div class="id-timer">
+              {{timer.timerid}}
+            </div>
+            <div class="timer-duration">
+              {{timer.duration}}
+            </div>
           </div>
         </div>
       </div>
-
     </div>
+    <div class="timer-actually">
+      <div class="timer-list">
+        <div class="title">
+          Таймеры
+        </div>
+        <div class="timer-head">
+          <div class="id-timer">
+            №
+          </div>
+          <div class="id-timer">
+            ID
+          </div>
+          <div class="timer-date-start">
+            Дата начало
+          </div>
+          <div class="timer-date-end">
+            Дата окончания
+          </div>
+          <div class="timer-duration">
+            Длительность
+          </div>
+          <div class="custom-buttons">
+
+          </div>
+        </div>
+        <div class="timer-body">
+          <div class="timer-item" v-for="(timer, index) in timerGrouppedByDate" :key="timer.id">
+            <div class="id-timer">
+              {{ index }}
+            </div>
+            <div class="id-timer">
+              {{timer.timerid}}
+            </div>
+            <div class="timer-date-start">
+              {{timer.datestart}}
+            </div>
+            <div class="timer-date-end">
+              {{timer.dateend}}
+            </div>
+            <div class="timer-duration">
+              {{timer.duration}}
+            </div>
+            <div class="custom-buttons">
+              <el-button
+                  @click="startTimer(timer)"
+                  class="start"
+                  type="primary"
+                  plain
+                  :disabled="!!startedindex && timer.timerid!==startedindex"
+              >
+                Старт
+              </el-button>
+              <el-button
+                  @click="stopTimer(timer)"
+                  class="start"
+                  type="danger"
+                  plain
+                  :disabled="!!startedindex && timer.timerid!==startedindex"
+              >
+                Стоп
+              </el-button>
+              <el-button
+                  @click="pauseTimer(timer)"
+                  class="start"
+                  type="warning"
+                  plain
+                  :disabled="!!startedindex && timer.timerid!==startedindex"
+              >
+                Пауза
+              </el-button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
   </div>
 </template>
 
 <style lang="scss" scoped>
+.title-timeout{
+  font-size: 1.5rem;
+  font-weight: 600;
+  margin-bottom: 15px;
+  width: 100%;
+  text-align: center;
+}
+.timer-actually{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
+.timer-closed{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
 .timer-wrapper{
   padding: 50px 0;
 }
 .timer-list{
-  margin: auto;
+  margin-bottom: 20px;
+  .title{
+    font-size: 14px;
+    text-align: center;
+    margin-bottom: 5px;
+  }
   .timer-head{
     display: flex;
     align-items: center;
@@ -147,7 +241,7 @@ onMounted(async ()=>{
    }
   }
   .id-timer{
-    width: 50px;
+    width: 80px;
   }
   .timer-date-start{
     width: 200px;
--- src/stores/globalSettings.js
+++ src/stores/globalSettings.js
@@ -4,13 +4,16 @@ export const useGlobalSettings = defineStore('', {
     state:()=>({
         timeout:0,
         timerList:[
-            {timerid:1, datestart:"30-06-2024", dateend:"31-08-2024", duration:"12:29:59"},
-            {timerid:2, datestart:"30-06-2024", dateend:"22-08-2024", duration:"01:29:59"},
-            {timerid:3, datestart:"30-06-2024", dateend:"15-08-2024", duration:"05:59:59"},
-            {timerid:4, datestart:"25-06-2024", dateend:"31-08-2024", duration:"12:30:59"},
-            {timerid:5, datestart:"30-06-2024", dateend:"22-08-2024", duration:"01:29:59"},
-            {timerid:6, datestart:"20-06-2024", dateend:"31-08-2024", duration:"12:30:59"},
-            {timerid:7, datestart:"30-06-2024", dateend:"31-08-2024", duration:"12:29:59"},
+            {timerid:112, datestart:"30-06-2024", dateend:"31-08-2024", duration:"12:29:59"},
+            {timerid:2215, datestart:"30-06-2024", dateend:"22-08-2024", duration:"01:29:59"},
+            {timerid:333, datestart:"30-06-2024", dateend:"15-08-2024", duration:"05:59:59"},
+            {timerid:4545, datestart:"25-06-2024", dateend:"31-08-2024", duration:"12:30:59"},
+            {timerid:588, datestart:"30-06-2024", dateend:"22-08-2024", duration:"01:29:59"},
+            {timerid:966, datestart:"20-06-2024", dateend:"31-08-2024", duration:"12:30:59"},
+            {timerid:313, datestart:"20-06-2024", dateend:"31-08-2024", duration:"01:01:03"},
+            {timerid:314, datestart:"20-06-2024", dateend:"31-08-2024", duration:"02:00:03"},
+            {timerid:215, datestart:"20-06-2024", dateend:"31-08-2024", duration:"01:00:05"},
+            {timerid:889, datestart:"30-06-2024", dateend:"31-08-2024", duration:"12:29:59"},
         ]
     }),
     actions:{
