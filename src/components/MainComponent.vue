ed71178fef2f1a96c98abb28e370e4d601dae93e
vohidimustafo <vohidiprofticket@gmail.com>
2024-07-10 15:20:02 +0300

    сгруппировка массива оп дате старта

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,23 +1,140 @@
 <script setup>
 import {useGlobalSettings} from "../stores/globalSettings.js";
-import {reactive} from "vue";
+import {computed, onMounted, reactive} from "vue";
 const global=useGlobalSettings()
-const timerGrouppedByDate=reactive([])
+const timerGrouppedByDate=computed(()=>{
+  let map = global.timerList.reduce((r, i) => {
+    r[i.datestart] = r[i.datestart] || [];
+    r[i.datestart].push(i);
+    return r;
+  }, {});
+  let arr=[]
+  for (var key in map) {
+    for (var elem in map[key]) {
+      arr.push(map[key][elem]);
+    }
+  }
+  console.log(arr)
+  return arr
+})
+function startTimer(){}
+function stopTimer(){}
+function pauseTimer(){}
 </script>
 <template>
   <div>
 
     <div class="timer-list">
       <div class="timer-head">
+        <div class="id-timer">
+          №
+        </div>
+        <div class="timer-date-start">
+          Дата начало
+        </div>
+        <div class="timer-date-end">
+          Дата окончания
+        </div>
+        <div class="timer-duration">
+          Длительность
+        </div>
+        <div class="custom-buttons">
 
+        </div>
       </div>
-      <div class="timer-item" v-for="timer in timerGrouppedByDate" :key="timer.id">
+      <div class="timer-body">
+        <div class="timer-item" v-for="timer in timerGrouppedByDate" :key="timer.id">
+          <div class="id-timer">
+            {{timer.id + 1}}
+          </div>
+          <div class="timer-date-start">
+            {{timer.datestart}}
+          </div>
+          <div class="timer-date-end">
+            {{timer.dateend}}
+          </div>
+          <div class="timer-duration">
+            {{timer.duration}}
+          </div>
+          <div class="custom-buttons">
+            <el-button
+                @click="startTimer"
+                class="start"
+                type="primary"
+                plain
+            >
+              Старт
+            </el-button>
+            <el-button
+                @click="stopTimer"
+                class="start"
+                type="danger"
+                plain
+            >
+              Стоп
+            </el-button>
+            <el-button
+                @click="pauseTimer"
+                class="start"
+                type="warning"
+                plain
+            >
+              Пауза
+            </el-button>
+          </div>
+        </div>
+      </div>
 
-      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-
+.timer-list{
+  max-width: 1200px;
+  margin: auto;
+  .timer-head{
+    display: flex;
+    align-items: center;
+    &>div{
+      height: 40px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border: 1px solid #dcdfe6;
+      box-sizing: border-box;
+      padding: 3px 8px;
+    }
+  }
+  .timer-body{
+   .timer-item{
+     display: flex;
+     align-items: center;
+     &>div{
+       height: 40px;
+       display: flex;
+       align-items: center;
+       justify-content: center;
+       border: 1px solid #dcdfe6;
+       box-sizing: border-box;
+       padding: 3px 8px;
+     }
+   }
+  }
+  .id-timer{
+    width: 50px;
+  }
+  .timer-date-start{
+    width: 200px;
+  }
+  .timer-date-end{
+    width: 200px;
+  }
+  .timer-duration{
+    width: 200px;
+  }
+  .custom-buttons{
+    width: 250px;
+  }
+}
 </style>
--- src/stores/globalSettings.js
+++ src/stores/globalSettings.js
@@ -5,9 +5,11 @@ export const useGlobalSettings = defineStore('', {
         timerList:[
             {id:0, datestart:"30-06-2024", dateend:"31-08-2024", duration:"12:29:59"},
             {id:1, datestart:"30-06-2024", dateend:"22-08-2024", duration:"01:29:59"},
-            {id:1, datestart:"30-06-2024", dateend:"15-08-2024", duration:"05:59:59"},
-            {id:2, datestart:"25-06-2024", dateend:"31-08-2024", duration:"12:30:59"},
-            {id:3, datestart:"20-06-2024", dateend:"31-08-2024", duration:"12:30:59"},
+            {id:2, datestart:"30-06-2024", dateend:"15-08-2024", duration:"05:59:59"},
+            {id:3, datestart:"25-06-2024", dateend:"31-08-2024", duration:"12:30:59"},
+            {id:4, datestart:"30-06-2024", dateend:"22-08-2024", duration:"01:29:59"},
+            {id:5, datestart:"20-06-2024", dateend:"31-08-2024", duration:"12:30:59"},
+            {id:6, datestart:"30-06-2024", dateend:"31-08-2024", duration:"12:29:59"},
         ]
     }),
     actions:{
